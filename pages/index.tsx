74e9bb04180766ebf489dfb096ae3df92b347bbc
jerrynim <tjerry3@naver.com>
2020-02-07 16:27:43 +0900

    [Fix] :wrench: eslint use doublequote

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import { NextPage } from "next";
 const index: NextPage = () => {
   return (
     <div>
-      <div>hi</div>
       <img src="https://media.giphy.com/media/h0cVMLhAiBtug/giphy.gif" alt="" />
     </div>
   );
