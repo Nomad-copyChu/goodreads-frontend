f4959ccbf1ee214c01054f160920fc6a8d4cad54
jerrynim <tjerry3@naver.com>
2020-02-15 17:59:19 +0900

    [Add] addpage paths  :heavy_plus_sign:

---
+++ pages/add/author.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const author: React.FC = () => {
+  return <div>hello world</div>;
+};
+
+export default author;
---
+++ pages/add/qoute.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const qoute: React.FC = () => {
+  return <div>hello world</div>;
+};
+
+export default qoute;
