89d38445bfdc952b7464085aa6f4201c6ded96da
jerrynim <tjerry3@naver.com>
2020-02-25 21:20:57 +0900

    [Change] font to noto sans cjk kr

--- style/GlobalStyle.ts
+++ style/GlobalStyle.ts
@@ -22,7 +22,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: Noto Sans KR;
   font-weight: normal;
   font-display: optional;
-  src: url('/fonts/NotoSansKR-Regular-Alphabetic.woff2') format("woff2");
+  src: url('/fonts/NotoSansCJKkr-Regular-Alphabetic.woff2') format("woff2");
   /* 한글 */
   unicode-range:U+0020-U+007E,U+1100-U+11F9,U+3000-U+303F,U+3131-U+318E,U+327F-U+327F,U+AC00-U+D7A3,U+FF01-U+FF60;
 }
@@ -31,7 +31,7 @@ const GlobalStyles = createGlobalStyle`
   font-family: Noto Sans KR;
   font-weight: bold;
   font-display: optional;
-  src: url('/fonts/NotoSansKR-Bold-Alphabetic.woff2') format("woff2");
+  src: url('/fonts/NotoSansCJKkr-Bold-Alphabetic.woff2') format("woff2");
   /* 한글 */
   unicode-range:U+0020-U+007E,U+1100-U+11F9,U+3000-U+303F,U+3131-U+318E,U+327F-U+327F,U+AC00-U+D7A3,U+FF01-U+FF60;
 }
@@ -55,7 +55,7 @@ const GlobalStyles = createGlobalStyle`
   unicode-range: U+0020-U+002F,U+0030-U+0039,U+003A-U+0040,U+0041-U+005A,U+005B-U+0060,U+0061-U+007A,U+007B-U+007E;
 }
   body {
-    font-family: Noto Sans,Noto Sans KR;
+    font-family: Noto Sans CJK KR,Noto Sans KR;
     background-color:${colors.beige_400};
     height: 100vh;
     line-height:1.4;
